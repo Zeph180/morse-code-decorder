ccd22206fc2722067410d37d96738f07a53fef79
Zeph180 <zephrichards1@gmail.com>
2023-04-12 17:40:39 +0300

    Solve morse code decorder

---
+++ decoder.rb
@@ -0,0 +1,25 @@
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3',
+  '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7',
+  '---..' => '8', '----.' => '9', '-----' => '0'
+}
+
+def my_decoder(morse_code)
+  words = morse_code.split('   ')
+  decoded_words = words.map do |word|
+    letters = word.split(' ')
+    puts letters
+    decoded_letters = letters.map do |letter|
+      MORSE_CODE[letter]
+    end
+    decoded_letters.join
+  end
+  decoded_words.join(' ')
+end
+
+puts my_decoder('- .... .. ...   .. ...   .-. .- ..- ... .')
